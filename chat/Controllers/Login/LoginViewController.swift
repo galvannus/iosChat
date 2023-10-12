a60d4d9b1c0a114cbe8bb690524046454262f5d1
Jorge Marcial <jorgemarcialg27@gmail.com>
2023-10-11 23:30:52 -0600

    Comenzando a configurar login

---
+++ chat/Controllers/Login/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  chat
+//
+//  Created by Jorge Alejndro Marcial Galvan on 11/10/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Log In"
+        view.backgroundColor = .white
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register", style: .done, target: self, action: #selector(didTapRegister))
+    }
+    
+    @objc private func didTapRegister(){
+        let vc = RegisterViewController()
+        vc.title = "Create Account"
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
+}
---
+++ chat/Controllers/Login/RegisterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterViewController.swift
+//  chat
+//
+//  Created by Jorge Alejndro Marcial Galvan on 11/10/23.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .link
+    }
+
+}

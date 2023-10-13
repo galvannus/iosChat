2efcd8283ddd165402eb6cd4a24e07e1d7ce7cbd
Jorge Marcial <jorgemarcialg27@gmail.com>
2023-10-12 23:02:28 -0600

    Agregué logo de la app en el centro del login

--- chat/Controllers/Login/LoginViewController.swift
+++ chat/Controllers/Login/LoginViewController.swift
@@ -8,19 +8,38 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    private var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Log In"
         view.backgroundColor = .white
-        
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register", style: .done, target: self, action: #selector(didTapRegister))
+
+        setUpView()
+        setUpLayout()
     }
-    
-    @objc private func didTapRegister(){
+
+    private func setUpView() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register", style: .done, target: self, action: #selector(didTapRegister))
+
+        imageView = UIImageView(image: UIImage(named: "logo"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+    }
+
+    private func setUpLayout() {
+        view.addSubview(imageView)
+
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+    }
+
+    @objc private func didTapRegister() {
         let vc = RegisterViewController()
         vc.title = "Create Account"
         navigationController?.pushViewController(vc, animated: true)
     }
-
 }

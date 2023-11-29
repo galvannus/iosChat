69a5ab47976056af38d254ecd1473abb52026af8
Jorge Marcial <jorgemarcialg27@gmail.com>
2023-11-28 19:12:05 -0600

    Agregado dark mode a NewConversation.

--- chat/Controllers/NewConversation/NewConversationViewController.swift
+++ chat/Controllers/NewConversation/NewConversationViewController.swift
@@ -20,7 +20,7 @@ class NewConversationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .systemBackground
 
         setUpView()
         setUpLayout()
